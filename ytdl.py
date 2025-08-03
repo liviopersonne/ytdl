8f3097eeaefef89dc76b92c422e99a13ba605794
livio <livio.personne@gmail.com>
2025-08-03 17:27:35 +0200

    download video segments with chapter name or timestamps

--- ytdl.py
+++ ytdl.py
@@ -7,31 +7,33 @@ TEMP_PATH = os.path.join(os.getcwd(), "temp")
 LAST_ID_PATH = os.path.join(os.getcwd(), "env", "last_id.txt")
 
 base_options = {
-    'verbose': False,
-    'updatetime': False,
-    'paths': {
-        "temp": TEMP_PATH,
-        "home": DEST_PATH
-    },
-    'outtmpl': "%(title)s.%(ext)s"
+    "verbose": False,
+    "updatetime": False,
+    "paths": {"temp": TEMP_PATH, "home": DEST_PATH},
+    "outtmpl": "%(title)s.%(ext)s",
+    "format_sort": ["proto:https"],
 }
 audio_options = base_options | {
-    'format': "bestaudio/best",
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }]
+    "format": "bestaudio/best",
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }
+    ],
 }
 video_options = base_options | {
-    'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+    "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
 }
 
+
 def empty():
-    print("Emptying temp folder", end = "\r")
+    print("Emptying temp folder", end="\r")
     for file in os.listdir(TEMP_PATH):
         os.remove(os.path.join(TEMP_PATH, file))
     print("Successfully emptied temp folder !")
+
 
 def playlist_items(start: str, end: str, reverse: bool):
     if reverse:
@@ -47,19 +49,20 @@ def playlist_items(start: str, end: str, reverse: bool):
         rep += ":-1"
     return rep
 
+
 def search_id_in_playlist(playlist_url, target_id):
     scraper_options = {
-        'verbose': False,
-        'quiet': True,
-        'extract_flat': True,
-        'playlistend': 100,
+        "verbose": False,
+        "quiet": True,
+        "extract_flat": True,
+        "playlistend": 100,
     }
     with yt_dlp.YoutubeDL(scraper_options) as playlist_scraper:
         playlist_info = playlist_scraper.extract_info(playlist_url, download=False)
-        playlist_entries = playlist_info.get('entries', [])
-        first_id = playlist_entries[0].get('id')
-        for (i, vid) in enumerate(playlist_entries):
-            if vid.get('id') == target_id:
+        playlist_entries = playlist_info.get("entries", [])
+        first_id = playlist_entries[0].get("id")
+        for i, vid in enumerate(playlist_entries):
+            if vid.get("id") == target_id:
                 if i == 0:
                     input("Already up to date !")
                     raise Exception("Already up to date !")
@@ -67,15 +70,33 @@ def search_id_in_playlist(playlist_url, target_id):
     input("Target id not found in playlist")
     raise Exception("Target id not found in playlist")
 
+
 def match_url(url: str):
     if url in MY_URLS:
         return MY_URLS[url]
     else:
         return url
 
-def download(playlist, url, audio_only, start, end, reverse):
+
+def clean_timestamp(timestamp: str):
+    if not timestamp:
+        return None
+    times = timestamp.strip().split(":")
+    if len(times) == 1:
+        return int(times[0])
+    if len(times) == 2:
+        return 60 * int(times[0]) + int(times[1])
+    if len(times) == 3:
+        return 3600 * int(times[0]) + 60 * int(times[1]) + int(times[2])
+
+
+def download(
+    playlist, url, audio_only, start, end, reverse, chapter, start_time, end_time
+):
     if len(os.listdir(TEMP_PATH)) > 0:
-        resume = input("A download was interrupted, would you like to finish it ? ") == "1"
+        resume = (
+            input("A download was interrupted, would you like to finish it ? ") == "1"
+        )
         if resume:
             raise NotImplementedError("Need to implement resumption of download")
         else:
@@ -85,33 +106,58 @@ def download(playlist, url, audio_only, start, end, reverse):
     else:
         options = video_options
     if playlist:
-        options['playlist_items'] = playlist_items(start, end, reverse)
-    
+        options["playlist_items"] = playlist_items(start, end, reverse)
+    else:
+        if chapter:
+            options["download_ranges"] = yt_dlp.utils.download_range_func([chapter], [])
+        else:
+            options["download_ranges"] = yt_dlp.utils.download_range_func(
+                [], [[clean_timestamp(start_time), clean_timestamp(end_time)]]
+            )
+        # if chapter:
+        #     options["download-sections"] = chapter
+        #     options['download_ranges'] = yt_dlp.utils.download_range_func([], [[10.0, 20.0]])
+        # else:
+        #     if start_time or end_time:
+        #         start_clean = start_time if start_time else "inf"
+        #         end_clean = end_time if end_time else "inf"
+        #         options["download-sections"] = f"*{start_clean}-{end_clean}"
+
     ytdl = yt_dlp.YoutubeDL(options)
     ytdl.download(url)
     ytdl.close()
+
 
 def download_in_terminal():
     playlist = input("Playlist ? ") == "1"
     url = match_url(input("Url: "))
     audio_only = input("Audio only ? ") == "1"
     start, end, reverse = None, None, None
+    start_time, end_time, chapter = None, None, None
     if playlist:
-        start = input("Start: ")    # Leave empty for start of playlist
-        end = input("End: ")        # Leave empty for end of playlist
+        start = input("Start: ")  # Leave empty for start of playlist
+        end = input("End: ")  # Leave empty for end of playlist
         reverse = input("Reverse ? ") == "1"
-    download(playlist, url, audio_only, start, end, reverse)
+    else:
+        chapter = input("Chapter name: ")
+        if chapter == "":
+            start_time = input("Start time: ")
+            end_time = input("End time: ")
+    download(
+        playlist, url, audio_only, start, end, reverse, chapter, start_time, end_time
+    )
+
 
 def update_music():
     with open(LAST_ID_PATH, "r") as infile:
         last_downloaded_id = infile.read()
-    new_ldi, end = search_id_in_playlist(MY_URLS['music'], last_downloaded_id)
-    download(True, MY_URLS['music'], True, "", end, True)
+    new_ldi, end = search_id_in_playlist(MY_URLS["music"], last_downloaded_id)
+    download(True, MY_URLS["music"], True, "", end, True)
     with open(LAST_ID_PATH, "w") as outfile:
         outfile.write(new_ldi)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         try:
             type_of_download = sys.argv[1]
@@ -122,7 +168,7 @@ if __name__ == '__main__':
             pass
 
         download_in_terminal()
-    
+
     except Exception as e:
         print(e)
         input("Exit...")
